26fa9b61ee900ddfe3728c6fc3cc328c75ab3c54
frankfang <frankfang1990@gmail.com>
2015-10-18 02:18:03 +0800

    add

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,7 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+
+git clone https://github.com/olivierverdier/zsh-git-prompt.git ~/bin/zsh-git-prompt;
+
+
+
